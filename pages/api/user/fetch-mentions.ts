f0f33d6ded1bccad62ed16601bd1a5f443bd2ce8
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2024-01-08 12:36:53 -0500

    Debug in prod

--- pages/api/user/fetch-mentions.ts
+++ pages/api/user/fetch-mentions.ts
@@ -5,10 +5,10 @@ const api_key = process.env.LAKE_API_KEY;
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   let { userId } = getAuth(req);
-  if(!userId){
+  /*if(!userId){
     res.status(401).send("Not logged in");
     return;
-  }
+  }*/
   let { teamid,name,league,favorites,page} = req.query;
   userId=userId||"";
   const {data}= await axios.get(`${process.env.NEXT_PUBLIC_LAKEAPI}/api/v41/findexar/user/fetch-mentions?api_key=${api_key}&userid=${userId}&teamid=${teamid}&name=${encodeURIComponent(name as string||"")}&league=${encodeURIComponent(league as string||"")}&favorites=${encodeURIComponent(favorites as string||"")}&page=${encodeURIComponent(page as string||"")}`);
