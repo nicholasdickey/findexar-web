4c84e0bc358116de319ccc2df4727c0e023ca76f
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2024-01-08 12:41:39 -0500

    Debug in prod3

--- pages/api/user/fetch-mentions.ts
+++ pages/api/user/fetch-mentions.ts
@@ -9,10 +9,14 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     res.status(401).send("Not logged in");
     return;
   }*/
-  let { teamid,name,league,favorites,page} = req.query;
+  let { teamid,name,league,myteam,page} = req.query;
   userId=userId||"";
-  const {data}= await axios.get(`${process.env.NEXT_PUBLIC_LAKEAPI}/api/v41/findexar/user/fetch-mentions?api_key=${api_key}&userid=${userId}&teamid=${teamid}&name=${encodeURIComponent(name as string||"")}&league=${encodeURIComponent(league as string||"")}&favorites=${encodeURIComponent(favorites as string||"")}&page=${encodeURIComponent(page as string||"")}`);
-  console.log("API: fetch-mentions",`${process.env.NEXT_PUBLIC_LAKEAPI}/api/v41/findexar/user/fetch-mentions?api_key=${api_key}&userid=${userId}&teamid=${teamid}&name=${encodeURIComponent(name as string||"")}&league=${encodeURIComponent(league as string||"")}&favorites=${encodeURIComponent(favorites as string||"")}&page=${encodeURIComponent(page as string||"")}`);
+  teamid=teamid||"";
+  name=name||"";
+  myteam=myteam||"0";
+  page=page||"0";
+  const {data}= await axios.get(`${process.env.NEXT_PUBLIC_LAKEAPI}/api/v41/findexar/user/fetch-mentions?api_key=${api_key}&userid=${userId}&teamid=${teamid}&name=${encodeURIComponent(name as string||"")}&league=${encodeURIComponent(league as string||"")}&myteam=${encodeURIComponent(myteam as string||"")}&page=${encodeURIComponent(page as string||"")}`);
+  console.log("API: fetch-mentions",`${process.env.NEXT_PUBLIC_LAKEAPI}/api/v41/findexar/user/fetch-mentions?api_key=${api_key}&userid=${userId}&teamid=${teamid}&name=${encodeURIComponent(name as string||"")}&league=${encodeURIComponent(league as string||"")}&myteam=${encodeURIComponent(myteam as string||"")}&page=${encodeURIComponent(page as string||"")}`);
   res.status(200).json(data);
 };
 export default handler;
